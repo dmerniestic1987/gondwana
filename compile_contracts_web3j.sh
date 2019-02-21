ab74a14ddd6f66035fa7a7bd5f27579eb515efbf
dmerniestic1987 <dmernies@uade.edu.ar>
2019-02-20 23:20:49 -0300

    Se agrega script para invocar contratos

--- compile_contracts_web3j.sh
+++ compile_contracts_web3j.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export PANGEA_J_SRC_MAIN_JAVA=../pangea-j/src/main/java
 web3j truffle generate --javaTypes ./build/contracts/VersusMatches.json -o $PANGEA_J_SRC_MAIN_JAVA -p com.betex.pangeaj.dao.ethereum
+web3j truffle generate --javaTypes ./build/contracts/BetexCore.json -o $PANGEA_J_SRC_MAIN_JAVA -p com.betex.pangeaj.dao.ethereum
