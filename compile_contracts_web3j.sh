d50ce16e308fa36e3cb63a26714747bc74359d76
dmerniestic1987 <dmernies@uade.edu.ar>
2019-06-06 23:46:08 -0300

    updated script

--- compile_contracts_web3j.sh
+++ compile_contracts_web3j.sh
@@ -2,7 +2,11 @@
 export PANGEA_J_SRC_MAIN_JAVA=../pangea-j/src/main/java
 export PANGEA_J_SC_PACKAGE=com.betex.pangeaj.dao.ethereum
 export BETEX_MOBILE_ANDROID_OUTPUT=./build/android/
-export BETEX_MOBILE_SC_PACKAGE=ar.com.betex.betexmobile.sc
+export BETEX_MOBILE_SC_PACKAGE=ar.com.betex.betexmobile.blockchain.sc
+
+echo "*** GENERANDO CLASES JAVA DE SC ***"
+rm -Rf $BETEX_MOBILE_ANDROID_OUTPUT
+mkdir $BETEX_MOBILE_ANDROID_OUTPUT
 #= = = = = pangea-j = = = = =
 web3j truffle generate --javaTypes ./build/contracts/VersusMatches.json -o $PANGEA_J_SRC_MAIN_JAVA -p $PANGEA_J_SC_PACKAGE
 web3j truffle generate --javaTypes ./build/contracts/BetexCore.json -o $PANGEA_J_SRC_MAIN_JAVA -p $PANGEA_J_SC_PACKAGE
