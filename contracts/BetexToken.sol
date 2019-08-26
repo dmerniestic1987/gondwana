b8eb836538dca8b0aa4892769c26d2d1ad0c4c71
dmernies <dmernies@atixlabs.com>
2019-08-26 12:48:48 -0300

    Se cmabia la cantidad de BTX emitidos porque daba buffer overflow

--- contracts/BetexToken.sol
+++ contracts/BetexToken.sol
@@ -3,12 +3,12 @@ pragma solidity 0.5.10;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 /**
- * @dev BetexToken son los tokens que se utilizarán para las apuestas. 
+ * @dev BetexToken son los tokens que se utilizarán para las apuestas.
  * La emisión origina es de 2^255 BTX y no tiene decimales. Los tokens
  * no son divisibles.
  */
-contract BetexToken is ERC20Detailed, ERC20Mintable {    
+contract BetexToken is ERC20Detailed, ERC20Mintable {
     constructor() public ERC20Detailed("Betex Token", "BTX", 18) {
-        _mint(msg.sender, (2**255) * (10 ** uint256(decimals())));
+        _mint(msg.sender, (2**32) * (10 ** uint256(decimals())));
     }
 }
